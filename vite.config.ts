9ce5a385271e2619135325b0efdd39814aba7cb3
Sangsu Song <dvlprsong@gmail.com>
2026-01-23 11:15:01 +0900

    chore: add bundle size analyzer

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    process.env.ANALYZE === 'true' &&
+      visualizer({
+        open: true,
+        filename: 'stats.html',
+        gzipSize: true,
+        brotliSize: true,
+      }),
+  ],
   base: '/home-renewed/',
 });
